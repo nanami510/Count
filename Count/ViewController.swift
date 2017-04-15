65facce5ad5b57fcd9324d265c5ed66e433cfbad
後藤奈々美 <gotonanami0510@yahoo.co.jp>
2017-04-15 15:04:33 +0900

    改行

--- Count/ViewController.swift
+++ Count/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var number: Int=0
+    @IBOutlet var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +21,58 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func plus(){
+    number=number+1
+        label.text=String(number)
+        
+        if number >= 10{
+           label.textColor=UIColor.red
+        }else if number <= -10{
+            label.textColor=UIColor.blue
+        }else{
+            label.textColor=UIColor.white
+        }
+    }
+    @IBAction func minus(){
+        number=number-1
+        label.text=String(number)
+        if number >= 10{
+            label.textColor=UIColor.red
+        }else if number <= -10{
+            label.textColor=UIColor.blue
+        }else{
+            label.textColor=UIColor.white
+        }
 
+        
 
-}
+    }
+    @IBAction func div(){
+        number=number/2
+        label.text=String(number)
+        if number >= 10{
+            label.textColor=UIColor.red
+        }else if number <= -10{
+            label.textColor=UIColor.blue
+        }else{
+            label.textColor=UIColor.white
+        }
+
+        
+    }
+    @IBAction func mul(){
+        number=number*2
+        label.text=String(number)
+        if number >= 10{
+            label.textColor=UIColor.red
+        }else if number <= -10{
+            label.textColor=UIColor.blue
+        }else{
+            label.textColor=UIColor.white
+        }
+
+       
+    }
+    }
+    
 
